5d3027a5c7207e49b0753bd8670b09ada53fd417
Melvin Tang <mxcodeyard@gmail.com>
2020-08-24 09:18:03 +1000

    remove logger in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import reducer from './reducers';
 
-const middleware = applyMiddleware(logger, thunk);
+const middlewares = [ thunk ];
 
-export default createStore(reducer, middleware);
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(require('redux-logger').default);
+}
+
+export default createStore(reducer, applyMiddleware(...middlewares));
