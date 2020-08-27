f7b29c9eaae241cc468d1f1e8da567a1e142aa8f
mxcodeyard <mxcodeyard@gmail.com>
2020-08-27 19:35:03 +1000

    fav page styling edited

--- src/pages/favourites/Favourites.js
+++ src/pages/favourites/Favourites.js
@@ -1,48 +1,50 @@
-import React from 'react';
-import './Favourites.scss';
-import { connect } from 'react-redux';
+import React from "react";
+import "./Favourites.scss";
+import { connect } from "react-redux";
 
-import RecipeItem from '../../components/recipe-item/RecipeItem';
+import RecipeItem from "../../components/recipe-item/RecipeItem";
 
-import { removeFavourite } from '../../redux/actions/recipe';
+import { removeFavourite } from "../../redux/actions/recipe";
 
 const Favourites = ({ favourites, removeFavourite }) => {
-	const clickRemove = (id) => {
-		removeFavourite(id);
-	};
+  const clickRemove = (id) => {
+    removeFavourite(id);
+  };
 
-	return (
-		<div className="favourite-page">
-			<div className="recipe-search">
-				<h2 className="title">Favourite Recipes</h2>
-			</div>
-			<div className="recipe-list container-xl">
-				{favourites.length > 0 ? (
-					favourites.map((item, index) => (
-						<RecipeItem key={index} {...item}>
-							<button type="button" className="btn btn-secondary" onClick={() => clickRemove(index)}>
-								Remove
-							</button>
-							<button
-								type="button"
-								name="favourite"
-								className="btn btn-secondary"
-								onClick={() => console.log('clicked Detail')}
-							>
-								Details
-							</button>
-						</RecipeItem>
-					))
-				) : (
-					<p>No Favourite Recipes Found</p>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="favourite-page">
+      <h2 className="title">Favourite Recipes</h2>
+      <div className="recipe-list container-xl">
+        {favourites.length > 0 ? (
+          favourites.map((item, index) => (
+            <RecipeItem key={index} {...item}>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => clickRemove(index)}
+              >
+                Remove
+              </button>
+              <button
+                type="button"
+                name="favourite"
+                className="btn btn-secondary"
+                onClick={() => console.log("clicked Detail")}
+              >
+                Details
+              </button>
+            </RecipeItem>
+          ))
+        ) : (
+          <p>No favourite recipes found.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = ({ recipe: { favourites } }) => ({
-	favourites
+  favourites,
 });
 
 export default connect(mapStateToProps, { removeFavourite })(Favourites);
