175bef08ae44701157e159f66b9263aca0ab5a97
mxcodeyard <mxcodeyard@gmail.com>
2020-09-01 04:35:20 +1000

    Update

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => res.send("Hello from server"));
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
