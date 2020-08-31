f91ada5a7a66f36c73483e17c896ea0942bcb1ed
mxcodeyard <mxcodeyard@gmail.com>
2020-09-01 09:08:51 +1000

    Initial commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = 4000;
 
-app.get("/", (req, res) => res.send("Hello from server"));
+app.use(express.json());
+
+app.use("/api/recipes", require("./routes/recipes"));
+app.use("/api/favourites", require("./routes/favourites"));
+app.use("/api/auth", require("./routes/authentication"));
+app.use("/api/users", require("./routes/users"));
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
