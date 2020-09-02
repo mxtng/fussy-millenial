06df62d644dc34d408bde64034fdf454bb1178df
mxcodeyard <mxcodeyard@gmail.com>
2020-09-03 06:49:18 +1000

    Update users api route v1

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => res.send('Users Route'));
+router.post('/register', (req, res) => {
+  res.send(req.body);
+});
+
+router.post('/login', (req, res) => {
+  res.send(req.body);
+});
 
 module.exports = router;
