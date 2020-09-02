ef27c1e3261893428def6bf0211e99faa7f8d464
mxcodeyard <mxcodeyard@gmail.com>
2020-09-03 09:43:48 +1000

    Update alert redux action

--- client/src/components/alert/Alert.js
+++ client/src/components/alert/Alert.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Alert = ({ alert }) => {
+const Alert = ({ alert, msg }) => {
   return !alert ? (
     ""
   ) : (
     <div className="alert alert-danger" role="alert">
-      Ingredients missing
+      {msg}
     </div>
   );
 };
 
-const mapStateToProps = ({ alert: { alert } }) => ({ alert });
+const mapStateToProps = ({ alert: { alert, msg } }) => ({ alert, msg });
 
 export default connect(mapStateToProps)(Alert);
