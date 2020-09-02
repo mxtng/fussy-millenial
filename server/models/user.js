0b70842864c6a4e1a0b0088ea951f72ba76f5f0f
mxcodeyard <mxcodeyard@gmail.com>
2020-09-03 08:39:52 +1000

    User db model added

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const moongoose = require("mongoose");
+
+const UserSchema = new moongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("user", UserSchema);
