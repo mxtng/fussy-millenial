7e6d218bfbfa3029871bb2126a60792845f1a0ec
mxcodeyard <mxcodeyard@gmail.com>
2020-09-02 16:19:56 +1000

    Update recipe api

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const apiKey = require("../config/db").apiKey;
 
 const Recipe = require("../models/recipes");
 
 router.post("/", async (req, res) => {
   try {
-    const { name, ingredients } = req.body;
+    const { ingredients, recipeCount } = req.body;
 
-    const newRecipe = Recipe({
-      name,
-      ingredients,
-    });
-
-    const recipe = await newRecipe.save();
-
-    res.json(recipe);
+    const fetchRecipes = await axios.get(
+      "https://api.spoonacular.com/recipes/findByIngredients",
+      {
+        params: {
+          apiKey,
+          ingredients,
+          number: recipeCount,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+      console.log(fetchRecipes.data);
+    res.send(fetchRecipes.data);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Error");
