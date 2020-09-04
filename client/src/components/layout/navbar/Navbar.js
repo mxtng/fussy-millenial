7a26b1dcd37fd4dfb9afc6728e6d98efcd673050
mxcodeyard <mxcodeyard@gmail.com>
2020-09-04 13:41:01 +1000

    Update login/logout status

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -1,49 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import './Navbar.scss';
+import "./Navbar.scss";
 
-const Navbar = () => (
-	<nav className="navbar block navbar-expand-md
-   navbar-dark bg-dark">
-		<div className="container-lg">
-			<input type="checkbox" id="navbar-toggler-checkbox"/>
-			<label
-				htmlFor="navbar-toggler-checkbox"
-				className="navbar-toggler-checkbox"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarTogglerDemo01"
-				aria-controls="navbarTogglerDemo01"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon" />
-			</label>
+const Navbar = ({ authenticated }) => {
+  console.log(authenticated);
+  return (
+    <nav
+      className="navbar block navbar-expand-md
+   navbar-dark bg-dark"
+    >
+      <div className="container-lg">
+        <input type="checkbox" id="navbar-toggler-checkbox" />
+        <label
+          htmlFor="navbar-toggler-checkbox"
+          className="navbar-toggler-checkbox"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </label>
 
-			<Link className="navbar-brand h1" to="/">
-				Fussy Millenial
-			</Link>
+        <Link className="navbar-brand h1" to="/">
+          Fussy Millenial
+        </Link>
 
-			<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-				<div className="navbar-nav">
-					<Link className="nav-link" to="/recipes">
-						Recipes
-					</Link>
-				</div>
-				<div className="navbar-nav ml-auto">
-					<Link className="nav-link" to="/favourites">
-						Favourites
-					</Link>
-					<Link className="nav-link" to="/register">
-						Register
-					</Link>
-					<Link className="nav-link" to="/signin">
-						Sign In
-					</Link>
-				</div>
-			</div>
-		</div>
-	</nav>
-);
-export default Navbar;
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <div className="navbar-nav">
+            <Link className="nav-link" to="/recipes">
+              Recipes
+            </Link>
+          </div>
+          <div className="navbar-nav ml-auto">
+            <Link className="nav-link" to="/favourites">
+              Favourites
+            </Link>
+            {!authenticated ? (
+              <Fragment>
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+                <Link className="nav-link" to="/signin">
+                  Sign In
+                </Link>
+              </Fragment>
+            ) : (
+              <Link className="nav-link" to="/signin">
+                Sign Out
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const mapStateToProps = ({ auth: { authenticated } }) => ({
+  authenticated,
+});
+
+export default connect(mapStateToProps)(Navbar);
