70cc0ff60ef409f3c9a1197ba23890ddd06c786f
mxcodeyard <mxcodeyard@gmail.com>
2020-09-07 06:54:08 +1000

    show other ingredients in recipe card

--- client/src/components/recipe-item/RecipeItem.js
+++ client/src/components/recipe-item/RecipeItem.js
@@ -5,6 +5,7 @@ const RecipeItem = ({
   image,
   title,
   usedIngredients: ingredients,
+  missedIngredients: others,
   children,
 }) => {
   return (
@@ -13,9 +14,15 @@ const RecipeItem = ({
         <img className="card-img-top" src={image} alt="" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <h6>Main ingredients:</h6>
+          <h6>Primary ingredients:</h6>
           <ul>
             {ingredients.map(({ name }, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+          <h6>Other ingredients:</h6>
+          <ul>
+            {others.map(({ name }, index) => (
               <li key={index}>{name}</li>
             ))}
           </ul>
