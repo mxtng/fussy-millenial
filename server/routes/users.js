c68bda64c443a41a34db8be44fd8bd634bdd3166
mxcodeyard <mxcodeyard@gmail.com>
2020-09-07 05:57:41 +1000

    user validation added

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,34 +5,57 @@ const jwt = require("jsonwebtoken");
 
 const secretKey = require("../config/db").secretKey;
 
+const { check, validationResult } = require("express-validator");
+
 const User = require("../models/user");
 
 // User Registration
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  try {
-    const user = new User({
-      name,
-      email,
-      password,
-    });
+router.post(
+  "/register",
+  [
+    check("name").not().isEmpty(),
+    check("email").isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+    const { name, email, password } = req.body;
 
-    await user.save();
+    try {
+      let user = await User.findOne({ email });
 
-    const token = jwt.sign({ id: user.id }, secretKey);
+      if (user) return res.status(400).send("Email already exists");
 
-    res.send(token);
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send("Registration Failed");
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const token = jwt.sign({ id: user.id }, secretKey);
+
+      res.send(token);
+    } catch (error) {
+      console.error(error.message);
+      res.status(400).send("Registration Failed");
+    }
   }
-});
+);
 
 // User Login
-router.post("/login", async (req, res) => {
+router.post("/login", [
+  check('email').isEmail(),
+  check('password').exists()
+],async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
