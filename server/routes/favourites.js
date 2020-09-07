b136809d2a784f3cb91edef677b30b2f1684d2ef
mxcodeyard <mxcodeyard@gmail.com>
2020-09-07 15:49:33 +1000

    favourite route edited

--- server/routes/favourites.js
+++ server/routes/favourites.js
@@ -1,6 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res) => res.send('Favourites Route'));
+const Recipe = require("../models/recipe");
+
+router.put("/", async (req, res) => {
+  try {
+    const { id, image, title, usedIngredients, missedIngredients } = req.body;
+
+    const recipe = new Recipe({
+      _id: id,
+      image,
+      title,
+      usedIngredients,
+      missedIngredients,
+    });
+
+    await recipe.save();
+
+    res.send(req.body);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = router;
