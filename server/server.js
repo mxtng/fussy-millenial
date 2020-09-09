03cd06866a497154f5f4bd67a8a276253bf5bc54
mxcodeyard <mxcodeyard@gmail.com>
2020-09-09 13:26:36 +1000

    fix initial site load

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const path = require("path");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 mongoose.connect(
-  process.env.MONGODB_URI || require("./config/db").db,
+  // process.env.MONGODB_URI || require("./config/db").db,
+  process.env.MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (error) => {
     if (error) {
@@ -20,7 +22,11 @@ app.use("/api/auth", require("./routes/authentication"));
 app.use("/api/users", require("./routes/users"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.resolve(__dirname, "../client/build/")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '../client/build/index.html');
+  });
 }
 
 const PORT = process.env.PORT || 4000;
