7cb1e23684a57628c1ecb04cd0fec323a9e67d53
mxcodeyard <mxcodeyard@gmail.com>
2020-09-11 17:21:31 +1000

    static files req fix

--- server/server.js
+++ server/server.js
@@ -26,7 +26,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../client/build/")));
 
   app.get("*", (req, res) => {
-    res.sendFile(__dirname + "../client/build/index.html");
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 
